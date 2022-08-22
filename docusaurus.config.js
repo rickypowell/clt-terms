b9377b2095ff822e9fff34189e828e1eda8477ba
Ricky Powell <rickypowell@users.noreply.github.com>
2022-08-21 19:08:32 -0500

    updated logo - renamed "My Site" to "Home" - replaced logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,10 +53,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Home',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Car Loan Talk Logo',
+          src: 'img/logo.png',
         },
         items: [
           {
